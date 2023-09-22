caf4d5f266788e11c0359792ad8d6a13b4945d58
wuwenjie <wenjiewu2017@163.com>
2023-09-22 14:28:38 +0800

    测试文件

---
+++ TEST/CMakeFiles/Server.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/project/Trluper/src/AbstractHandle.cc" "CMakeFiles/Server.dir/src/AbstractHandle.cc.o" "gcc" "CMakeFiles/Server.dir/src/AbstractHandle.cc.o.d"
+  "/home/project/Trluper/src/Connections.cc" "CMakeFiles/Server.dir/src/Connections.cc.o" "gcc" "CMakeFiles/Server.dir/src/Connections.cc.o.d"
+  "/home/project/Trluper/src/DataProcess.cc" "CMakeFiles/Server.dir/src/DataProcess.cc.o" "gcc" "CMakeFiles/Server.dir/src/DataProcess.cc.o.d"
+  "/home/project/Trluper/src/FApplycations.cc" "CMakeFiles/Server.dir/src/FApplycations.cc.o" "gcc" "CMakeFiles/Server.dir/src/FApplycations.cc.o.d"
+  "/home/project/Trluper/src/Message.cc" "CMakeFiles/Server.dir/src/Message.cc.o" "gcc" "CMakeFiles/Server.dir/src/Message.cc.o.d"
+  "/home/project/Trluper/src/Server.cc" "CMakeFiles/Server.dir/src/Server.cc.o" "gcc" "CMakeFiles/Server.dir/src/Server.cc.o.d"
+  "/home/project/Trluper/test/main.cc" "CMakeFiles/Server.dir/test/main.cc.o" "gcc" "CMakeFiles/Server.dir/test/main.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TEST/CMakeFiles/Server.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Server.
+# This may be replaced when dependencies are built.
