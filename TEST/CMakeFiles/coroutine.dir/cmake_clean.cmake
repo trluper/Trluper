6dfd9715833f9003b52c817efbf6cd1e7d7b4f5b
wuwenjie <wenjiewu2017@163.com>
2023-12-20 17:26:29 +0800

    添加了http协议

---
+++ TEST/CMakeFiles/coroutine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coroutine.dir/test/test_coroutine.cc.o"
+  "CMakeFiles/coroutine.dir/test/test_coroutine.cc.o.d"
+  "coroutine"
+  "coroutine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coroutine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TEST/CMakeFiles/http_package.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/http_package.dir/test/test_httpPackage.cc.o"
+  "CMakeFiles/http_package.dir/test/test_httpPackage.cc.o.d"
+  "http_package"
+  "http_package.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/http_package.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
